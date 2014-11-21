7e6af8188ddaab6a3b1c98e40800fb2cea4f47bf
mertcanekiz <ekiz.mertcan@gmail.com>
2014-11-21 23:01:18 +0200

    Add comments to Menu.cc

--- src/Menu.cc
+++ src/Menu.cc
@@ -2,9 +2,10 @@
 #include "Application.h"
 
 Menu::Menu(int id) : GameState(id),
-		     buttons{ Button("start", Application::SCREEN_WIDTH - Button::WIDTH - 24, 210),
-	Button("options", Application::SCREEN_WIDTH - Button::WIDTH - 24, 235),
-	Button("exit", Application::SCREEN_WIDTH - Button::WIDTH - 24, 260) }
+		     buttons{  //Initialize buttons in the appropriate positions
+                             Button("start", Application::SCREEN_WIDTH - Button::WIDTH - 24, 210),
+			     Button("options", Application::SCREEN_WIDTH - Button::WIDTH - 24, 235),
+			     Button("exit", Application::SCREEN_WIDTH - Button::WIDTH - 24, 260)}
 {
     menubg = nullptr;
 }
@@ -33,24 +34,29 @@ void Menu::input(SDL_Event event)
 
     for(auto &b : buttons)
     {
-	if(x > b.getX() && x < b.getX() + Button::WIDTH &&
-	   y > b.getY() && y < b.getY() + Button::HEIGHT)
+	if(x > b.getX() && x < b.getX() + Button::WIDTH &&  //If the mouse is within a button's bounds
+	   y > b.getY() && y < b.getY() + Button::HEIGHT)   //////////////////////////////////////////
 	{
 	    if(!mousedown)
 	    {
+		//State is "hover" if mouse button is not down
 		b.changeState(Button::STATE_HOVER);
 	    }
 	    if(mousedown)
 	    {
+		//State is "press" if mouse button is down
 		b.changeState(Button::STATE_PRESS);
 	    }
 	    if(event.type == SDL_MOUSEBUTTONUP && event.button.button == SDL_BUTTON_LEFT && b.getLastState() == Button::STATE_PRESS)
 	    {
+		//"Click" happens if the mouse button is released within the button and the button's
+		//last state was "press"
 		b.click();
 	    }
 	}
 	else
 	{
+	    //If mouse is not inside the button, revert to state "normal"
 	    b.changeState(Button::STATE_NORMAL);
 	}
     }
